15ec4d605e89f0adfd9ae161ed212f462f286ef5
annie91annie91 <79952901+annie91annie91@users.noreply.github.com>
2021-03-17 09:41:34 +0800

    Add files via upload

---
+++ 11.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int a[5]={0,10,20,30,40};
+void printAll()
+{
+    for(int i=0;i<10;i++){
+        printf("%d",a[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int *p=&a[2];
+    *p=200;
+        printAll();
+    int *p2=p+4;
+    *p=666;
+        printAll();
+    p--;
+    *p=555;
+        printAll();
+        return 0;
+}
